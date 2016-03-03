f1921d334325c3bc02776ab5b6e00716cf905465
Dustin Blackman <dev@dustinblackman.com>
2016-03-03 08:47:59 -0500

    fix docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -5,4 +5,4 @@ docker build -t dustinblackman/xgo-1.6.0 .
 cd ../go-1.6.x
 docker build -t dustinblackman/xgo-1.6.x .
 cd ../go-latest
-docker build -t dustinblackman/xgo-latest
+docker build -t dustinblackman/xgo-latest .
